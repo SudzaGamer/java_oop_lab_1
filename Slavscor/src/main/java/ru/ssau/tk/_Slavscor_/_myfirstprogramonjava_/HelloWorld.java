2d048ee1b516baf97cf44da316109e57fe2ed15d
Slavik <44920182+SudzaGamer@users.noreply.github.com>
2025-09-22 22:02:49 +0400

    Delete Slavscor directory

--- Slavscor/src/main/java/ru/ssau/tk/_Slavscor_/_myfirstprogramonjava_/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package ru.ssau.tk._Slavscor_._myfirstprogramonjava_;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
