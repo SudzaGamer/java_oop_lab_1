b839e7e9cb02839cd44b574b7992196a6caba168
Konev_V <v.konev.hack.1@gmail.com>
2025-09-22 21:15:50 +0400

    Добавлен первый Java-класс

---
+++ Slavscor/src/main/java/ru/ssau/tk/_Slavscor_/_myfirstprogramonjava_/HelloWorld.java
@@ -0,0 +1,7 @@
+package ru.ssau.tk._Slavscor_._myfirstprogramonjava_;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
